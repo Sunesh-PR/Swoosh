2aed5af302f2c5b884a9bb452ba54cea78858e8e
suneshchamp <suneshchamp@gmail.com>
2018-06-26 13:09:11 +0530

    cleared warnings and intented the getstarted btn

--- app/src/main/java/com/example/sunesh/swoosh/activity_league.kt
+++ app/src/main/java/com/example/sunesh/swoosh/activity_league.kt
@@ -8,5 +8,7 @@ class activity_league : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
     }
+
 }
